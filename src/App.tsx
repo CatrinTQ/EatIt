ecf9296464d975555c282a2a22a2d2e8e9cdbed4
David Kjellstrand <david.kjellstrand@gmail.com>
2025-08-14 13:39:27 +0200

    Davids Test 2

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>EATIT</h1>
+      <h2>david</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

2c7783b72bd16c131dd87a46408309e31748a374
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-08-15 11:54:35 +0200

    feat: recipe cards

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,14 +1,20 @@
 type RecipeCardProps = {
   title: string;
   img: string;
+  ingredients: string[];
   instructions: string[];
 };
 
-export const RecipeCard = ({ title, img, instructions }: RecipeCardProps) => {
+export const RecipeCard = ({ title, img, ingredients, instructions }: RecipeCardProps) => {
   return (
     <>
       <h2>{title}</h2>
       <img src={img} alt={`Picture of ${title}`} />
+      <ul>
+        {ingredients.map((step, index) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ul>
       <ol>
         {instructions.map((step, index) => (
           <li key={index}>{step}</li>

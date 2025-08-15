b6bb96593824112b31fa3bc1ff156f77774552e7
Olivia <166169277+oliviaalmseger@users.noreply.github.com>
2025-08-15 14:34:40 +0200

    länkar funkar

--- src/components/OneRecipe.tsx
+++ src/components/OneRecipe.tsx
@@ -8,13 +8,12 @@ export const OneRecipe = () => {
   const index = Number(id);
   const recipe = recipies[index];
 
-
-
   return (
     <div className="one-recipe">
 
       <RecipeCard
         title={recipe.title}
+        description={recipe.description}
         img={recipe.img}
         ingredients={recipe.ingredients}
         instructions={recipe.instructions}
